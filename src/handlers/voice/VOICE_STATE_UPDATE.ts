3e9b9f6095229e764d07c5003f491e3b8b9ada33
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-04-13 21:53:36 +0000

    fix: voice state update handler for join voice channel event

--- src/handlers/voice/VOICE_STATE_UPDATE.ts
+++ src/handlers/voice/VOICE_STATE_UPDATE.ts
@@ -13,42 +13,36 @@ import {
 } from "../../util/utils.ts";
 
 export async function handleVoiceStateUpdate(data: DiscordGatewayPayload) {
-  const payload = snakeKeysToCamelCase<VoiceState>(
-    data.d as DiscordVoiceState,
-  );
+  const payload = snakeKeysToCamelCase<VoiceState>(data.d as DiscordVoiceState);
   if (!payload.guildId) return;
 
   const guild = await cacheHandlers.get("guilds", payload.guildId);
   if (!guild) return;
 
   const member = payload.member
-    ? await structures.createDiscordenoMember(
-      camelKeysToSnakeCase<DiscordGuildMemberWithUser>(payload),
-      guild.id,
-    )
+    ? await structures.createDiscordenoMember(camelKeysToSnakeCase(payload.member), guild.id)
     : await cacheHandlers.get("members", payload.userId);
   if (!member) return;
 
   // No cached state before so lets make one for em
   const cachedState = guild.voiceStates.get(payload.userId);
 
-  guild.voiceStates.set(
-    payload.userId,
-    payload,
-  );
+  guild.voiceStates.set(payload.userId, payload);
 
   await cacheHandlers.set("guilds", payload.guildId, guild);
 
   if (cachedState?.channelId !== payload.channelId) {
     // Either joined or moved channels
     if (payload.channelId) {
-      if (cachedState?.channelId) { // Was in a channel before
+      if (cachedState?.channelId) {
+        // Was in a channel before
         eventHandlers.voiceChannelSwitch?.(
           member,
           payload.channelId,
-          cachedState.channelId,
+          cachedState.channelId
         );
-      } else { // Was not in a channel before so user just joined
+      } else {
+        // Was not in a channel before so user just joined
         eventHandlers.voiceChannelJoin?.(member, payload.channelId);
       }
     } // Left the channel

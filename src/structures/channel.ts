0c1a5c180337fc9915c5b3ac8ec172837a36edc1
ITOH <to@itoh.at>
2021-06-29 14:09:41 +0200

    idk

--- src/structures/channel.ts
+++ src/structures/channel.ts
@@ -1,26 +1,8 @@
 import { eventHandlers } from "../bot.ts";
-import { cache } from "../cache.ts";
-import { channelOverwriteHasPermission } from "../helpers/channels/channel_overwrite_has_permission.ts";
-import { cloneChannel } from "../helpers/channels/clone_channel.ts";
-import { deleteChannel } from "../helpers/channels/delete_channel.ts";
-import { deleteChannelOverwrite } from "../helpers/channels/delete_channel_overwrite.ts";
-import { editChannel } from "../helpers/channels/edit_channel.ts";
-import { editChannelOverwrite } from "../helpers/channels/edit_channel_overwrite.ts";
-import { sendMessage } from "../helpers/messages/send_message.ts";
-import { disconnectMember } from "../helpers/mod.ts";
 import type { Channel } from "../types/channels/channel.ts";
-import { DiscordChannelTypes } from "../types/channels/channel_types.ts";
-import type { ModifyChannel } from "../types/channels/modify_channel.ts";
-import type { DiscordOverwrite, Overwrite } from "../types/channels/overwrite.ts";
-import type { CreateMessage } from "../types/messages/create_message.ts";
-import type { PermissionStrings } from "../types/permissions/permission_strings.ts";
+import type { DiscordOverwrite } from "../types/channels/overwrite.ts";
 import { snowflakeToBigint } from "../util/bigint.ts";
-import { Collection } from "../util/collection.ts";
 import { createNewProp } from "../util/utils.ts";
-import { DiscordenoGuild } from "./guild.ts";
-import { DiscordenoMember } from "./member.ts";
-import { DiscordenoMessage } from "./message.ts";
-import { DiscordenoVoiceState } from "./voice_state.ts";
 
 const CHANNEL_SNOWFLAKES = ["id", "guildId", "lastMessageId", "ownerId", "applicationId", "parentId"];
 
@@ -53,54 +35,6 @@ const baseChannel: Partial<DiscordenoChannel> = {
       rtcRegion: this.rtcRegion,
       videoQualityMode: this.videoQualityMode,
     } as Channel;
-  },
-  get guild() {
-    return cache.guilds.get(this.guildId!);
-  },
-  get messages() {
-    return cache.messages.filter((m) => m.channelId === this.id!);
-  },
-  get mention() {
-    return `<#${this.id!}>`;
-  },
-  get voiceStates() {
-    return this.guild?.voiceStates.filter((voiceState) => voiceState.channelId === this.id!);
-  },
-  get connectedMembers() {
-    const voiceStates = this.voiceStates;
-    if (!voiceStates) return undefined;
-
-    return new Collection(voiceStates.map((vs) => [vs.userId, cache.members.get(vs.userId)]));
-  },
-  get isNewsChannel() {
-    return this.type === DiscordChannelTypes.GuildNews;
-  },
-  get isGuildTextBasedChannel() {
-    return [DiscordChannelTypes.GuildNews, DiscordChannelTypes.GuildText].includes(this.type!);
-  },
-  send(content) {
-    return sendMessage(this.id!, content);
-  },
-  disconnect(memberId) {
-    return disconnectMember(this.guildId!, memberId);
-  },
-  delete(reason) {
-    return deleteChannel(this.id!, reason);
-  },
-  editOverwrite(id, options) {
-    return editChannelOverwrite(this.guildId!, this.id!, id, options);
-  },
-  deleteOverwrite(id) {
-    return deleteChannelOverwrite(this.guildId!, this.id!, id);
-  },
-  hasPermission(overwrites, permissions) {
-    return channelOverwriteHasPermission(this.guildId!, this.id!, overwrites, permissions);
-  },
-  edit(options, reason) {
-    return editChannel(this.id!, options, reason);
-  },
-  clone(reason) {
-    return cloneChannel(this.id!, reason);
   },
 };
 
@@ -169,64 +103,8 @@ export interface DiscordenoChannel
   applicationId?: bigint;
   /** Id of the parent category for a channel (each parent category can contain up to 50 channels) */
   parentId?: bigint;
-  // GETTERS
-
-  /**
-   * Gets the guild object for this channel.
-   *
-   * ⚠️ ADVANCED: If you use the custom cache, these will not work for you. Getters can not be async and custom cache requires async.
-   */
-  guild?: DiscordenoGuild;
-  /**
-   * Gets the messages from cache that were sent in this channel
-   *
-   * ⚠️ ADVANCED: If you use the custom cache, these will not work for you. Getters can not be async and custom cache requires async.
-   */
-  messages: Collection<bigint, DiscordenoMessage>;
-  /** The mention of the channel */
-  mention: string;
-  /**
-   * Gets the voice states for this channel
-   *
-   * ⚠️ ADVANCED: If you use the custom cache, these will not work for you. Getters can not be async and custom cache requires async.
-   */
-  voiceStates?: Collection<bigint, DiscordenoVoiceState>;
-  /**
-   * Gets the connected members for this channel undefined if member is not cached
-   *
-   * ⚠️ ADVANCED: If you use the custom cache, these will not work for you. Getters can not be async and custom cache requires async.
-   */
-  connectedMembers?: Collection<bigint, DiscordenoMember | undefined>;
-  /** Whether the channel is a news channel. */
-  isNewsChannel: boolean;
-  /** Whether the channel is a news or text channel in a guild. */
-  isGuildTextBasedChannel: boolean;
 
   // METHODS
-
-  /** Send a message to the channel. Requires SEND_MESSAGES permission. */
-  send(content: string | CreateMessage): ReturnType<typeof sendMessage>;
-  /** Disconnect a member from a voice channel. Requires MOVE_MEMBERS permission. */
-  disconnect(memberId: bigint): ReturnType<typeof disconnectMember>;
-  /** Delete the channel */
-  delete(reason?: string): ReturnType<typeof deleteChannel>;
-  /** Edit a channel Overwrite */
-  editOverwrite(overwriteId: bigint, options: Omit<Overwrite, "id">): ReturnType<typeof editChannelOverwrite>;
-  /** Delete a channel Overwrite */
-  deleteOverwrite(overwriteId: bigint): ReturnType<typeof deleteChannelOverwrite>;
-  /** Checks if a channel overwrite for a user id or a role id has permission in this channel */
-  hasPermission(
-    overwrites: (Omit<DiscordOverwrite, "id" | "allow" | "deny"> & {
-      id: bigint;
-      allow: bigint;
-      deny: bigint;
-    })[],
-    permissions: PermissionStrings[]
-  ): ReturnType<typeof channelOverwriteHasPermission>;
-  /** Edit the channel */
-  edit(options: ModifyChannel, reason?: string): ReturnType<typeof editChannel>;
-  /** Create a new channel with the same properties */
-  clone(reason?: string): ReturnType<typeof cloneChannel>;
   /** Returns the Channel object json value */
   toJSON(): Channel;
 }
--- src/structures/role.ts
+++ src/structures/role.ts
@@ -1,16 +1,7 @@
 import { eventHandlers } from "../bot.ts";
-import { cache } from "../cache.ts";
-import { deleteRole } from "../helpers/roles/delete_role.ts";
-import { editRole } from "../helpers/roles/edit_role.ts";
-import { CreateGuildRole } from "../types/guilds/create_guild_role.ts";
-import { Errors } from "../types/discordeno/errors.ts";
 import type { Role } from "../types/permissions/role.ts";
 import { snowflakeToBigint } from "../util/bigint.ts";
-import { Collection } from "../util/collection.ts";
-import { highestRole } from "../util/permissions.ts";
 import { createNewProp } from "../util/utils.ts";
-import { DiscordenoGuild } from "./guild.ts";
-import { DiscordenoMember } from "./member.ts";
 
 const ROLE_SNOWFLAKES = ["id", "botId", "integrationId", "guildId"];
 
@@ -26,51 +17,11 @@ const roleToggles = {
 };
 
 const baseRole: Partial<DiscordenoRole> = {
-  get guild() {
-    return cache.guilds.get(this.guildId!);
-  },
   get hexColor() {
     return this.color!.toString(16);
   },
-  get members() {
-    return cache.members.filter((m) => m.guilds.some((g) => g.roles.includes(this.id!)));
-  },
   get mention() {
     return `<@&${this.id}>`;
-  },
-
-  // METHODS
-  delete() {
-    return deleteRole(this.guildId!, this.id!);
-  },
-  edit(options) {
-    return editRole(this.guildId!, this.id!, options);
-  },
-  higherThanRole(roleId: bigint, position?: number) {
-    // If no position try and find one from cache
-    if (!position) position = this.guild?.roles.get(roleId)?.position;
-    // If still none error out.
-    if (!position) {
-      throw new Error(
-        "role.higherThanRoleId() did not have a position provided and the role or guild was not found in cache. Please provide a position like role.higherThanRoleId(roleId, position)"
-      );
-    }
-
-    // Rare edge case handling
-    if (this.position === position) {
-      return this.id! < roleId;
-    }
-
-    return this.position! > position;
-  },
-  async higherThanMember(memberId: bigint) {
-    const guild = this.guild;
-    if (!guild) throw new Error(Errors.GUILD_NOT_FOUND);
-
-    if (guild.ownerId === memberId) return false;
-
-    const memberHighestRole = await highestRole(guild, memberId);
-    return this.higherThanRole!(memberHighestRole.id, memberHighestRole.position);
   },
   get hoist() {
     return Boolean(this.bitfield! & roleToggles.hoist);
@@ -84,6 +35,9 @@ const baseRole: Partial<DiscordenoRole> = {
   get isNitroBoostRole() {
     return Boolean(this.bitfield! & roleToggles.isNitroBoostRole);
   },
+
+  // METHODS
+
   toJSON() {
     return {
       guildId: this.guildId?.toString(),
@@ -159,25 +113,13 @@ export interface DiscordenoRole extends Omit<Role, "tags" | "id" | "permissions"
 
   // GETTERS
 
-  /** The guild where this role is. If undefined, the guild is not cached */
-  guild?: DiscordenoGuild;
   /** The hex color for this role. */
   hexColor: string;
-  /** The cached members that have this role */
-  members: Collection<bigint, DiscordenoMember>;
   /** The @ mention of the role in a string. */
   mention: string;
 
   // METHODS
 
-  /** Delete the role */
-  delete(): ReturnType<typeof deleteRole>;
-  /** Edits the role */
-  edit(options: CreateGuildRole): ReturnType<typeof editRole>;
-  /** Checks if this role is higher than another role. */
-  higherThanRole(roleId: bigint, position?: number): boolean;
-  /** Checks if the role has a higher position than the given member */
-  higherThanMember(memberId: bigint): Promise<boolean>;
   /** Convert to json friendly role with guild id */
   toJSON(): Role & { guildId: string };
 }
